046ecb8dd8d54a0f92ffde8ec7a580aa219f76e3
DimaMayTeacher@gmail.com <DimaMayTeacher@gmail.com@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2012-06-24 05:58:32 +0000

    added script files

---
+++ trunk/CDHInstall/scripts/startCDH.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+cd $HADOOP_HOME/sbin
+./start-dfs.sh
+
+# wait for safe mode to turn off before starting the rest of the products
+function checkSafemode(){
+  hdfs dfsadmin -safemode get | grep ON
+}
+echo "Waiting for safemode to turn OFF"
+while [[ "`checkSafemode`" != "" ]]; do
+  echo "..."
+  sleep 5
+done
+echo "Safemode is off! Continuing..."
+
+./start-yarn.sh
+./mr-jobhistory-daemon.sh start historyserver
+cd $HBASE_HOME/bin
+./start-hbase.sh
+cd $OOZIE_HOME/bin
+#./oozie-start.sh
