51a08b68a4dba53768f07fc9c11370ac62c9a008
DimaMayTeacher@gmail.com <DimaMayTeacher@gmail.com@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2012-06-24 05:58:32 +0000

    added script files

---
+++ CDHInstall/scripts/hadoop-env.sh
@@ -0,0 +1,31 @@
+export TRAINING_HOME=/home/hadoop/Training
+export HADOOP_LOGS=$TRAINING_HOME/logs
+export HADOOP_WORK_DIR=$TRAINING_HOME/hadoop_work
+export HADOOP_PID_DIR=$HADOOP_WORK_DIR/pids
+export PLAY_AREA=/home/hadoop/Training/play_area
+
+export JAVA_HOME=$TRAINING_HOME/jdk1.6.0_29
+export JDK_HOME=$JAVA_HOME
+
+export CDH_HOME=$TRAINING_HOME/CDH4
+export HADOOP_HOME=$CDH_HOME/hadoop-2.0.0-cdh4.0.0
+export HBASE_HOME=$CDH_HOME/hbase-0.92.1-cdh4.0.0
+
+export HADOOP_CONF_DIR=$HADOOP_HOME/conf
+
+export HADOOP_LOG_DIR=$HADOOP_LOGS/hadoop
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_MAPRED_HOME
+export YARN_CONF_DIR=$HADOOP_LOG_DIR
+export YARN_LOG_DIR=$HADOOP_LOGS/yarn
+export OOZIE_HOME=$CDH_HOME/oozie-3.1.3-cdh4.0.0
+export OOZIE_URL=http://localhost:11000/oozie
+
+export HADOOP_PID_DIR=$HADOOP_PID_DIR
+
+export HADOOP_SAMPLES_SRC=/home/hadoop/Training/eclipse_workspace/HadoopSamples/src/main
+
+export PATH=$PATH:./:/home/hadoop/Training/scripts:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HBASE_HOME/bin:$OOZIE_HOME/bin
+
---
+++ CDHInstall/scripts/stopCDH.sh
@@ -0,0 +1,11 @@
+cd $HBASE_HOME/bin
+./stop-hbase.sh
+sleep 2
+cd $HADOOP_HOME/sbin
+./mr-jobhistory-daemon.sh stop historyserver
+./stop-yarn.sh
+./stop-dfs.sh
+cd $OOZIE_HOME/bin
+#./oozie-stop.sh
+
+
