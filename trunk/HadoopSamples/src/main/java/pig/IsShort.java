2c430febd06c2a201125fd97e5cd957bd00bb762
dimamayteacher <dimamayteacher@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2012-07-29 18:48:47 +0000

    Updates to 'Pig Advanced' Lecture

--- trunk/HadoopSamples/src/main/java/pig/IsShort.java
+++ trunk/HadoopSamples/src/main/java/pig/IsShort.java
@@ -2,28 +2,24 @@ package pig;
 
 import java.io.IOException;
 
-import org.apache.log4j.Logger;
 import org.apache.pig.FilterFunc;
 import org.apache.pig.data.Tuple;
 
 public class IsShort extends FilterFunc{
-	private Logger log = Logger.getLogger(IsShort.class);
-	
+	private static final int MAX_CHARS = 15;
 	@Override
 	public Boolean exec(Tuple tuple) throws IOException {
 		if ( tuple == null || tuple.isNull() || tuple.size() == 0 ){
 			return false;
 		}
 		Object obj = tuple.get(0);
-		log.info("processing tuple [" + obj + "]");
 		if ( obj instanceof String){
 			String st = (String)obj;
-			if ( st.length() > 15 ){
+			if ( st.length() > MAX_CHARS ){
 				return false;
 			}
 			return true;
 		}
 		return false;
 	}
-
 }
---
+++ trunk/HadoopSamples/src/main/java/pig/IsShortWithSchema.java
@@ -0,0 +1,42 @@
+package pig;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.pig.FilterFunc;
+import org.apache.pig.FuncSpec;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.impl.logicalLayer.FrontendException;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
+
+public class IsShortWithSchema extends FilterFunc {
+	private static final int MAX_CHARS = 15;
+	@Override
+	public Boolean exec(Tuple tuple) throws IOException {
+		if (tuple == null || tuple.isNull() || tuple.size() == 0) {
+			return false;
+		}
+		Object obj = tuple.get(0);
+		if (obj instanceof String) {
+			String st = (String) obj;
+			if (st.length() > MAX_CHARS) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<FuncSpec> getArgToFuncMapping() throws FrontendException {
+		List<FuncSpec> schemaSpec = new ArrayList<FuncSpec>();
+		FuncSpec fieldSpec = new FuncSpec(this.getClass().getName(),
+				new Schema(new FieldSchema(null, DataType.CHARARRAY)));
+		schemaSpec.add(fieldSpec);
+
+		return schemaSpec;
+	}
+}
