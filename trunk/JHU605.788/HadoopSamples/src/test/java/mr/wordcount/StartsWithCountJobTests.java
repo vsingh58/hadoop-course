a5a3e76a992b286835505fc1639c9e49e3c851e3
DimaMayTeacher <DimaMayTeacher@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2013-01-26 22:39:33 +0000

    JHU605.788: Code samples for      HADP_08 Running MapReduce Jobs      HADP_09 MapReduce Features

---
+++ trunk/JHU605.788/HadoopSamples/src/test/java/mr/wordcount/StartsWithCountJobTests.java
@@ -0,0 +1,63 @@
+package mr.wordcount;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StartsWithCountJobTests {
+    
+    private File inputFile =
+      new File("./target/test/input.txt");
+    private File output = new File("./target/test-result/");
+
+    @Before
+    public void setUpTest() throws IOException{
+      FileUtils.deleteQuietly(inputFile);
+      FileUtils.deleteQuietly(output);
+      FileUtils.writeStringToFile(inputFile,
+              "this is just a test, yes it is");
+    }
+    
+    @Test
+    public void testRun() throws Exception {
+      Configuration conf = new Configuration();
+      conf.set("mapreduce.framework.name", "local");
+      conf.set("fs.default.name", "file:///");
+        
+      StartsWithCountJob underTest = new StartsWithCountJob();
+      underTest.setConf(conf);
+        
+      int exitCode = underTest.run(
+        new String[]{inputFile.getAbsolutePath(),
+            output.getAbsolutePath()});
+      assertEquals("Returned error code.", 0, exitCode);
+      assertTrue(new File(output, "_SUCCESS").exists());
+      Map<String,Integer> resAsMap =
+        getResultAsMap(new File(output, "part-r-00000"));
+        
+      assertEquals(5, resAsMap.size());
+      assertEquals(2, resAsMap.get("t").intValue());
+      assertEquals(3, resAsMap.get("i").intValue());
+      assertEquals(1, resAsMap.get("j").intValue());
+      assertEquals(1, resAsMap.get("a").intValue());
+      assertEquals(1, resAsMap.get("y").intValue());
+    }
+    private Map<String, Integer> getResultAsMap(File file) 
+            throws IOException {
+
+      Map<String, Integer> result = new HashMap<String, Integer>();
+      String contentOfFile = FileUtils.readFileToString(file);
+      for (String line : contentOfFile.split("\n")){
+        String [] tokens = line.split("\t");
+        result.put(tokens[0], Integer.parseInt(tokens[1]));
+      }
+      return result;
+    }
+}
