73762ed660dd76de3130cdf43acdeaa1baec4d3d
DimaMayTeacher <DimaMayTeacher@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2013-03-09 23:11:56 +0000

    HADP_17: MRUnit and LocalJobRunner testing examples

---
+++ trunk/JHU605.788/HadoopSamples/src/test/java/mr/wordcount/ReviewAvroInputMapperTest.java
@@ -0,0 +1,75 @@
+package mr.wordcount;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import mr.reviews.ReviewJob;
+import mr.reviews.fsstruct.avro.ReviewAvroMapper;
+import mr.reviews.fsstruct.avro.model.ReviewAvro;
+import mr.reviews.fsstruct.avro.model.ReviewKeyAvro;
+import mr.reviews.fsstruct.avro.model.ReviewReportAvro;
+
+import org.apache.avro.hadoop.io.AvroSerialization;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReviewAvroInputMapperTest {
+
+    private Configuration conf;
+    
+    @Before
+    public void before(){
+        conf = new Configuration();
+        conf.setStrings("io.serializations", new String[] {
+                AvroSerialization.class.getName()
+            });
+        AvroSerialization.setKeyWriterSchema(conf, ReviewKeyAvro.SCHEMA$);
+        AvroSerialization.setValueWriterSchema(conf, ReviewReportAvro.SCHEMA$);
+    }
+    
+    @Test
+    public void testValueFoundInReview() throws IOException {
+        ReviewAvro inputReview = ReviewAvro.newBuilder()
+                .setUser("user1").setTimestamp(13330820823030l)
+                .setText("This is where review would go").build();
+        String keyword = "where";
+        conf.set(ReviewJob.PROP_FIND_VALUE, keyword);
+        
+        ReviewKeyAvro foundKey = ReviewKeyAvro.newBuilder()
+                .setUser(inputReview.getUser())
+                .setKeyword(keyword).build();
+        
+        ReviewReportAvro foundValue = ReviewReportAvro.newBuilder()
+                .setKeyword(keyword).setNumReviews(1)
+                .setReviews(Arrays.asList(inputReview))
+                .setUser(inputReview.getUser()).build();
+        
+        new MapDriver<AvroKey<ReviewAvro>, NullWritable, AvroKey<ReviewKeyAvro>, AvroValue<ReviewReportAvro>>()
+            .withConfiguration(conf)
+            .withMapper(new ReviewAvroMapper())
+            .withInput(new AvroKey<ReviewAvro>(inputReview), NullWritable.get())
+            .withOutput(new AvroKey<ReviewKeyAvro>(foundKey), new AvroValue<ReviewReportAvro>(foundValue))
+            .runTest();
+    }
+
+    
+    @Test
+    public void testNoValueFound() throws IOException {
+        ReviewAvro inputReview = ReviewAvro.newBuilder()
+                .setUser("user1").setTimestamp(13330820823030l)
+                .setText("This is where review would go").build();
+        String keyword = "wontfind";
+        conf.set(ReviewJob.PROP_FIND_VALUE, keyword);
+        
+        new MapDriver<AvroKey<ReviewAvro>, NullWritable, AvroKey<ReviewKeyAvro>, AvroValue<ReviewReportAvro>>()
+            .withConfiguration(conf)
+            .withMapper(new ReviewAvroMapper())
+            .withInput(new AvroKey<ReviewAvro>(inputReview), NullWritable.get())
+            .runTest();
+    }
+}
---
+++ trunk/JHU605.788/HadoopSamples/src/test/java/mr/wordcount/StartsWithCountJob_DistCacheTest.java
@@ -0,0 +1,73 @@
+package mr.wordcount;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.filecache.DistributedCache;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StartsWithCountJob_DistCacheTest {
+    
+    private File inputFile =
+      new File("./target/test/input.txt");
+    private File output = new File("./target/test-result/");
+    private File distCacheFile = 
+      new File("./target/" + StartsWithCountMapper_DistCache.EXCLUDE_FILE);
+    
+    @Before
+    public void setUpTest() throws IOException{
+      FileUtils.deleteQuietly(inputFile);
+      FileUtils.deleteQuietly(output);
+      FileUtils.writeStringToFile(inputFile,
+              "this is just a test, yes it is");
+      if(distCacheFile.exists()){
+          FileUtils.forceDelete(distCacheFile);
+      }
+      FileUtils.write(distCacheFile, "a");
+    }
+    
+    @Test
+    public void testWithLocalJobRunner() throws Exception {
+      Configuration conf = new Configuration();
+      conf.set("mapreduce.framework.name", "local");
+      conf.set("fs.default.name", "file:///");
+        
+      StartsWithCountJob_DistCache underTest = new StartsWithCountJob_DistCache();
+      underTest.setConf(conf);
+      DistributedCache.addCacheFile(distCacheFile.toURI(), conf);
+        
+      int exitCode = underTest.run(
+        new String[]{inputFile.getAbsolutePath(),
+            output.getAbsolutePath()});
+      assertEquals("Returned error code.", 0, exitCode);
+      assertTrue(new File(output, "_SUCCESS").exists());
+      Map<String,Integer> resAsMap =
+        getResultAsMap(new File(output, "part-r-00000"));
+        
+      assertEquals(4, resAsMap.size());
+      assertEquals(2, resAsMap.get("t").intValue());
+      assertEquals(3, resAsMap.get("i").intValue());
+      assertEquals(1, resAsMap.get("j").intValue());
+      assertEquals(1, resAsMap.get("y").intValue());
+    }
+    private Map<String, Integer> getResultAsMap(File file) 
+            throws IOException {
+
+      Map<String, Integer> result = new HashMap<String, Integer>();
+      String contentOfFile = FileUtils.readFileToString(file);
+      for (String line : contentOfFile.split("\n")){
+        String [] tokens = line.split("\t");
+        result.put(tokens[0], Integer.parseInt(tokens[1]));
+      }
+      return result;
+    }
+}
