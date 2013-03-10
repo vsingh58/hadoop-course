2efb2d91ebcfeb3c47616ce2c58ef194a7b140a5
DimaMayTeacher <DimaMayTeacher@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2013-03-10 17:19:30 +0000

    HADP_17: Added Mini DFS cluster Example

--- trunk/JHU605.788/HadoopSamples/src/main/java/mr/reviews/fsstruct/support/FsHelper.java
+++ trunk/JHU605.788/HadoopSamples/src/main/java/mr/reviews/fsstruct/support/FsHelper.java
@@ -1,10 +1,14 @@
 package mr.reviews.fsstruct.support;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.commons.lang.Validate;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -12,7 +16,6 @@ import org.apache.hadoop.io.IOUtils;
 public class FsHelper {
 
     private final FileSystem fs;
-    @SuppressWarnings("unused")
     private final Configuration conf; 
     public FsHelper(Configuration conf) throws IOException{
         Validate.notNull(conf);
@@ -20,7 +23,7 @@ public class FsHelper {
         this.fs = FileSystem.get(conf);
     }
     
-    public byte [] readBytes(Path path) throws IOException{
+    public byte [] readBytes(Path path){
         FSDataInputStream in = null;
         try {
             in = fs.open(path);
@@ -28,8 +31,27 @@ public class FsHelper {
             byte[] buffer = new byte[(int)bytesLen];
             IOUtils.readFully(in, buffer, 0, buffer.length);
             return buffer;
+        } catch (IOException e ){
+            throw new RuntimeException("Failed to read from [" + path + "]", e);
         } finally {
             IOUtils.closeStream(in);
         }
+    }
+
+    public void writeStringToFile(Path path, String string) {
+        InputStream in = new BufferedInputStream(new ByteArrayInputStream(string.getBytes()));
+        FSDataOutputStream out = null;
+        try {
+            out = fs.create(path);
+            IOUtils.copyBytes(in, out, conf);
+        } catch (IOException e ){
+            throw new RuntimeException("Failed to write string to [" + path + "]", e);
+        } finally {
+            IOUtils.closeStream(out);
+        }
+    }
+
+    public String readStringFroomFile(Path path) {
+        return new String(readBytes(path));
     }
 }
---
+++ trunk/JHU605.788/HadoopSamples/src/test/java/mr/wordcount/MiniClusterStartsWithCountJobTest.java
@@ -0,0 +1,81 @@
+package mr.wordcount;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import mr.reviews.fsstruct.support.FsHelper;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.HdfsConfiguration;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.server.MiniYARNCluster;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MiniClusterStartsWithCountJobTest {
+
+    private Configuration conf = new HdfsConfiguration(new YarnConfiguration());
+    private MiniDFSCluster dfsCluster;
+    private MiniYARNCluster yarnCluster;
+
+    @Before
+    public void beforeTest() throws IOException {
+        dfsCluster = new MiniDFSCluster.Builder(conf).build();
+        dfsCluster.waitClusterUp();
+
+        yarnCluster = new MiniYARNCluster("MiniClusterStartsWithCountJobTest", // testName
+                1, // number of node managers
+                1, // number of local log dirs per node manager
+                1); // number of hdfs dirs per node manager
+        yarnCluster.init(conf);
+        yarnCluster.start();
+    }
+
+    @After
+    public void afterTest() {
+        dfsCluster.shutdown();
+        yarnCluster.stop();
+    }
+
+    @Test
+    public void testWithLocalJobRunner() throws Exception {
+        FsHelper fsHelper = new FsHelper(dfsCluster.getConfiguration(0));
+        Path input = new Path("/tests/MiniClusterStartsWithCountJobTest/input/in.txt");
+        Path output = new Path("/tests/MiniClusterStartsWithCountJobTest/output/");
+        fsHelper.writeStringToFile(input, "first line\nsecond line\nthird line");
+
+        StartsWithCountJob underTest = new StartsWithCountJob();
+        underTest.setConf(conf);
+
+        int exitCode = underTest.run(new String[] { input.toString(), output.toString() });
+        assertEquals("Returned error code.", 0, exitCode);
+
+        FileSystem fs = dfsCluster.getFileSystem();
+        assertTrue(fs.exists(new Path(output, "_SUCCESS")));
+        String outputAsStr = fsHelper.readStringFroomFile(new Path(output, "part-r-00000"));
+        Map<String, Integer> resAsMap = getResultAsMap(outputAsStr);
+
+        assertEquals(4, resAsMap.size());
+        assertEquals(1, resAsMap.get("f").intValue());
+        assertEquals(1, resAsMap.get("s").intValue());
+        assertEquals(1, resAsMap.get("t").intValue());
+        assertEquals(3, resAsMap.get("l").intValue());
+    }
+
+    private Map<String, Integer> getResultAsMap(String outputAsStr) throws IOException {
+        Map<String, Integer> result = new HashMap<String, Integer>();
+        for (String line : outputAsStr.split("\n")) {
+            String[] tokens = line.split("\t");
+            result.put(tokens[0], Integer.parseInt(tokens[1]));
+        }
+        return result;
+    }
+}
