0cd8770870c38a25bfb7675f9d475dfa5d1ffa7c
DimaMayTeacher <DimaMayTeacher@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2013-04-05 03:34:53 +0000

    Reduce Side Join example

---
+++ trunk/JHU605.788/HadoopSamples/src/main/java/mr/reviews/joins/ReduceSideJoinUsersLikesMapper.java
@@ -0,0 +1,20 @@
+package mr.reviews.joins;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ReduceSideJoinUsersLikesMapper extends Mapper<LongWritable, Text, Text, Text> {
+    private final Text joinKey = new Text();
+    private final Text outputValue = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        joinKey.set(value.toString().split(",")[0]);
+        outputValue.set("R" + value.toString());
+        context.write(joinKey, outputValue);
+    }
+
+}
