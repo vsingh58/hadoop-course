8d5e02432d539760bcb24e4eba7a4013a6912853
dimamayteacher@gmail.com <dimamayteacher@gmail.com@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2014-01-06 05:22:17 +0000

    Added jobs to runJobs script

--- JHU605.788/HadoopSamples/src/scripts/run/runJobs.sh
+++ JHU605.788/HadoopSamples/src/scripts/run/runJobs.sh
@@ -62,15 +62,50 @@ function printStats(){
    echo "##########################################################"
 }
 
+##############################
+# Compression
+hdfs dfs -rm /training/data/jdkSrc/jdkAppendedSrc.txt
+wget -O jdkAppendedSrc.tar.gz "http://goo.gl/l1JXZP"
+tar xvf jdkAppendedSrc.tar.gz
+hdfs dfs -mkdir /training/data/jdkSrc/
+hdfs dfs -put jdkAppendedSrc.txt /training/data/jdkSrc/
+rm jdkAppendedSrc.tar.gz jdkAppendedSrc.txt
+execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar compression.CompareCompression /training/data/jdkSrc/jdkAppendedSrc.txt"
 
+##############################
 # File Based Data Structures
+wget -O reviews-xml.tar.gz "http://goo.gl/zKhPTw"
+tar xvf reviews-xml.tar.gz
+hdfs dfs -mkdir /training/data/reviews-xml
+hdfs dfs -put reviews-xml/* /training/data/reviews-xml/
+rm reviews-xml.tar.gz*
+rm reviews-xml -rf
 
 reivewsInputXml="$resources/mr/reviews/fsstruct/ReviewsJob-xmlInput.xml"
 
 # must comment out as it generates lots of splits (2,999) and runs for over an hour
 #execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.SimpleTextXmlJob -conf $resources/mr/reviews/fsstruct/ReviewsJob-xmlInput.xml"
 
-#execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.SimpleTextXmlJob_CombineFileInputFormat -conf $reivewsInputXml"
+execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.SimpleTextXmlJob_CombineFileInputFormat -conf $reivewsInputXml"
+
+execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.seq.ReviewSequenceFileWriter -conf $resources/mr/reviews/fsstruct/seq/SeqCreation.xml"
+execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.seq.SequenceFileReader /training/data/reviews-seq/reviews.seqfile 2"
+
+execCommand "HADOOP_CLASSPATH=$PLAY_AREA/HadoopSamples.jar \
+yarn jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples*.jar sort \
+  -libjars $PLAY_AREA/HadoopSamples.jar \
+  -r 1 \
+  -inFormat org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat \
+  -outFormat org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat \
+  -outKey org.apache.hadoop.io.LongWritable \
+  -outValue mr.reviews.model.ReviewWritable \
+  /training/data/reviews-seq/reviews.seqfile /training/out/seq-to-map/"
+execCommand "hdfs dfs -mv /training/out/seq-to-map/part-r-00000 /training/out/seq-to-map/data"
+execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.mf.MapFileFix /training/out/seq-to-map/"
+execCommand "yarn jar $PLAY_AREA/HadoopSamples.jar mr.reviews.fsstruct.mf.MfReader /training/out/seq-to-map/"
+
+hdfs dfs -rm -r /training/data/reviews-seq/
+
 
 ##############################
 # Avro
