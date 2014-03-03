b9540c0f986253848b9ed155c38e308c976199d9
dimamayteacher@gmail.com <dimamayteacher@gmail.com@e545791c-357a-b5f4-ca7c-e60b7435d23d>
2014-03-03 04:43:14 +0000

    added MapReduce job implemented in groovy, Added compression to MapFile generation recipe

--- trunk/JHU605.788/HadoopSamples/src/scripts/run/examplesStreaming.sh
+++ trunk/JHU605.788/HadoopSamples/src/scripts/run/examplesStreaming.sh
@@ -15,4 +15,10 @@ execCommand "yarn jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.j
     -output /training/out/streaming/ -mapper countMap_withReporting.py -combiner countReduce.py -reducer countReduce.py"
 hdfs dfs -rm -r /training/out/streaming/
 
+groovyJar="/home/hadoop/.m2/repository/org/codehaus/groovy/groovy-all/2.2.2/groovy-all-2.2.2.jar"
+execCommand "HADOOP_CLASSPATH=${groovyJar} \
+    yarn jar $PLAY_AREA/HadoopSamples.jar mr.wordcount.groovy.CountGroovyJob -libjars ${groovyJar} \
+    examples_input/books/hamlet.txt /training/playArea/wordCount/"
+hdfs dfs -rm -r /training/playArea/wordCount/
+
 printStats
